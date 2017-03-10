1207d24a8588db89c6cdf40411bbabc3e6a7e6a2
mezzanine510 <mezzanine510@gmail.com>
2017-03-10 15:16:01 -0800

    Initial menu system and transition animations

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float timeToLoadNextLevel;
+
+	void Start()
+	{
+		Invoke("LoadNextLevel", timeToLoadNextLevel);
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		SceneManager.LoadScene (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
