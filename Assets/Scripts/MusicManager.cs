0fb575fb75cd5ad096a31894c6dead91dd6dfd4f
mezzanine510 <mezzanine510@gmail.com>
2017-03-18 18:36:44 -0700

    Added: background grid for levels; Fixed: music player

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour {
 
@@ -22,7 +23,7 @@ public class MusicManager : MonoBehaviour {
 	{
 		AudioClip thisLevelMusic = levelMusicChangeArray[level];
 
-		if (thisLevelMusic) // If there's some music attached to the array:
+		if (thisLevelMusic && audioSource.clip != thisLevelMusic) // If there's some music attached to the array:
 		{
 			audioSource.clip = thisLevelMusic;
 			audioSource.loop = true;
